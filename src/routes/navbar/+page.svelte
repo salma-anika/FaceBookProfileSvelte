1fda32c85349cfe6b977b612ca4d33e546d55667
salma-anika <anikaavaa@gmail.com>
2023-03-03 02:04:47 +0600

    fix icons

---
+++ src/routes/navbar/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import FacebookLogo from '../../icons/facebookLogo.svelte';
+import SearchIcon from '../../icons/searchIcon.svelte';
+import Plus from '../../icons/plus.svelte';
+import Messenger from '../../icons/messenger.svelte';
+import DownArrow from '../../icons/downArrow.svelte';
+import Bell from '../../icons/bell.svelte';
+import Home from '../../icons/home.svelte';
+import Watch from '../../icons/watch.svelte';
+import Groups from '../../icons/groups.svelte';
+import Feed from '../../icons/feed.svelte';
+</script>
+<div>
+    <div class="h-14 flex justify-between w-screen shadow-fb px-4 py-2">
+      <div class="flex ">
+        <FacebookLogo />
+        <div class="h-10 w-64 ml-2 flex items-center rounded-full bg-fFill p-3">
+          <SearchIcon />
+          <input
+            class="text-fGrey ml-2 bg-fFill focus:outline-none"
+            placeholder="Search Facebook"
+          />
+        </div>
+      </div>
+    </div>
+</div>
