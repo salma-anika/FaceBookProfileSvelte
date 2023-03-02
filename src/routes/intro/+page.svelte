cfef6abb49164ed9f6906b2be2937dae3a0f2ef7
salma-anika <anikaavaa@gmail.com>
2023-03-03 02:30:28 +0600

    start intro section and add cover

---
+++ src/routes/intro/+page.svelte
@@ -0,0 +1,9 @@
+<div class="px-44 shadow">
+    <div class="relative h-96 rounded-b flex justify-center max-[450px]:w-[600px]">
+        <img
+        src="src/OIP.jpg"
+        class="object-cover w-full h-full rounded-b"
+        alt="cover"
+      />
+      </div>
+      </div>
