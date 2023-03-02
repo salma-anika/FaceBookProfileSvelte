0f98876e63c9092c986a6da13d208ee72a2edec1
salma-anika <anikaavaa@gmail.com>
2023-03-03 02:32:07 +0600

    add profile pic

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -5,5 +5,14 @@
         class="object-cover w-full h-full rounded-b"
         alt="cover"
       />
+      <div class="absolute -bottom-6">
+        <img
+          src="src/pro.jpg"
+          class="object-cover border-4 border-white w-40 h-40 rounded-full"
+          alt="cover"
+        />
       </div>
+    </div>
       </div>
+
+      
