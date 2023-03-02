42bbf86fa08493ac1e66baa3c55c7d3e66c248e6
salma-anika <anikaavaa@gmail.com>
2023-03-03 02:33:38 +0600

    add profile name

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -12,7 +12,11 @@
           alt="cover"
         />
       </div>
+    
     </div>
+    <div class="text-center mt-6 text-3xl font-bold text-fBlack max-[450px]:hidden">
+        Salma Anika
+      </div>
       </div>
 
       
