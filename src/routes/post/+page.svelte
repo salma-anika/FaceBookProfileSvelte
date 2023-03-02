61fd7f76525ebec09a991ee55c57719db28d4f7d
salma-anika <anikaavaa@gmail.com>
2023-03-03 02:43:55 +0600

    add post area

---
+++ src/routes/post/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import DownArrow from '../../icons/downArrow.svelte';
+import Phone from '../../icons/phone.svelte';
+import Friend from '../../icons/friend.svelte';
+import More from '../../icons/more.svelte';
+</script>
+<div class="border border-fGrey mt-6 border-opacity-10" />
+<div class="flex justify-between px-8">
+  <div class="flex items-center">
+    <div class="px-4 py-5 text-fBlue border-b-4 border-fBlue">
+      Posts
+    </div>
+    <div class="px-4 py-5 text-fGrey">
+      About 
+    </div>
+    <div class="px-4 py-5 text-fGrey">
+      Friends 
+    </div>
+    </div>
+    </div>
