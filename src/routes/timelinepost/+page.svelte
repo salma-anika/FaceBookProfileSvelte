14c742ccaebec1fdc8e69505ab221ae0c08f1c60
salma-anika <anikaavaa@gmail.com>
2023-03-04 01:50:12 +0600

    add icons in timeline post

---
+++ src/routes/timelinepost/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import SuitCase from '../../icons/suitCase.svelte';
+import RSS from '../../icons/rss.svelte';
+import Pin from '../../icons/pin.svelte';
+import HomeAlt from '../../icons/homeAlt.svelte';
+import Hat from '../../icons/hat.svelte';
+import More from '../../icons/more.svelte';
+</script>
