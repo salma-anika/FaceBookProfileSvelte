27f2b4eb39559c48e8145cc94efb86ab66166d28
salma-anika <anikaavaa@gmail.com>
2023-03-04 01:59:30 +0600

    add  timeline post

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
     import "../app.css";
     import Navbar from '../routes/navbar/+page.svelte';
     import Intro from '../routes/intro/+page.svelte';
+    import Timelinepost from '../routes/timelinepost/+page.svelte'
   </script>
   <Navbar/>
   <Intro/>
-
+<Timelinepost/>
   <slot />
