e1d62b6bb2deeb73b94a2a96000d0c6416e6b034
salma-anika <anikaavaa@gmail.com>
2023-03-05 22:18:23 +0600

    set tablist

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,143 @@
     import Navbar from '../routes/navbar/+page.svelte';
     import Intro from '../routes/intro/+page.svelte';
     import Tab from '../routes/tab/+page.svelte'
+    import Tabs from '../routes/tabs/+page.svelte'
+    import Tablist from '../routes/tablist/+page.svelte'
+    import  Tabpanel from '../routes/tabpanel/+page.svelte'
+    import More from "../icons/more.svelte";
   </script>
   <Navbar/>
   <Intro/>
-<Tab/>
+  <div class='container px-44'>
+    <Tab>
+        
+        <Tablist>
+            <Tab>OverView</Tab>
+            <Tab> Work and education</Tab>
+            <Tab>Places lived</Tab>
+            <Tab> Contact and basic info</Tab>
+            <Tab>Details about you</Tab>
+        </Tablist>
+    
+        <Tabpanel>
+            <h2 class="content"><Intro/> </h2>
+        </Tabpanel>
+    
+        <Tabpanel>
+            <h2 class="content"> 
+                <div class="shadow-fb rounded w-full bg-white p-4">
+
+                    <div class="mt-1 flex items-start ">
+                      
+                      <span class="ml-2 ">
+                        Astha IT Limited Bangladesh{' '}
+                 
+                      </span>
+                      <div class=' items-end  justify-end ml-[385px] '> <More /></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+            
+                      <span class="ml-2">Studied at Bangladesh Army University </span>
+                      <div class=' items-end justify-end ml-[315px]'> <More/></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+                    
+                      <span class="ml-2">Intern at Sun IT Limited Rajshahi </span>
+                      <div class=' items-end justify-end ml-[360px]'> <More/></div>
+                    </div>
+                    </div>
+                         
+                         
+            </h2>
+        </Tabpanel>
+    
+        <Tabpanel>
+            <h2 class="content">
+                <div class="shadow-fb rounded w-full bg-white p-4">
+
+
+                    <div class="mt-4 flex items-center">
+                      
+                      <span class="ml-2">
+                        Lives in <b>Rajshahi</b>, Bangladesh{' '}
+                      </span>
+                      <div class=' items-end justify-end ml-[380px]'> <More /></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+            
+                      <span class="ml-2">
+                        From  <b>Rajshahi</b>, Bangladesh{' '}
+                      </span>
+                      <div class=' items-end ml-[393px] justify-end '> <More/></div>
+                    </div>
+                 
+                         </div>
+                         
+                 
+            </h2>
+        </Tabpanel>
+        <Tabpanel>
+            <h2 class="content">
+                <div class="shadow-fb rounded w-full bg-white p-4">
+
+                    <div class="mt-1 flex items-start ">
+                      
+                      <span class="ml-2 ">
+                        Astha IT Limited Bangladesh{' '}
+                 
+                      </span>
+                      <div class=' items-end  justify-end ml-[385px] '> <More/></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+                
+                      <span class="ml-2">Studied at Bangladesh Army University </span>
+                      <div class=' items-end justify-end ml-[315px]'> <More/></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+            
+                      <span class="ml-2">Intern at Sun IT Limited Rajshahi </span>
+                      <div class=' items-end justify-end ml-[360px]'> <More/></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+                
+                      <span class="ml-2">
+                        Lives in <b>Rajshahi</b>, Bangladesh{' '}
+                      </span>
+                      <div class=' items-end justify-end ml-[380px]'> <More/></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+            
+                      <span class="ml-2">
+                        From  <b>Rajshahi</b>, Bangladesh{' '}
+                      </span>
+                      <div class=' items-end ml-[393px] justify-end '> <More /></div>
+                    </div>
+                    <div class="mt-4 flex items-center">
+            
+                      <span class="ml-2">
+                        Followed by <b>97 people</b> {' '}
+                      </span>
+                      <div class=' items-end ml-[420px] justify-end '> <More /></div>
+                    </div>
+                    </div>
+                         
+                 
+                         
+                 
+            </h2>
+        </Tabpanel>
+        <Tabpanel>
+            <h2 class="content">
+                <Intro/>
+                         
+                 
+            </h2>
+        </Tabpanel>
+
+    </Tab>
+    
+ 
+    </div>
+   
+
   <slot />
