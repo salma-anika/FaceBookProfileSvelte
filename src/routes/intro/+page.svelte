f4db27d4bf7481757edd8920e58b6c8ca5a5d27f
salma-anika <anikaavaa@gmail.com>
2023-03-12 01:49:54 +0600

    response fix

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import Post from '../../routes/post/+page.svelte'
 </script>
-<div class="">
+<div class="shadow">
     <div class="relative h-96 rounded-b flex justify-center max-[450px]:w-[900px] max-[850px]:w-[1200px] max-[450px]:m-auto ">
         <img
         src="src/OIP.jpg"
@@ -17,7 +17,7 @@
       </div>
     
     </div>
-    <div class="text-center mt-6 text-3xl font-bold text-fBlack max-[450px]:hidden max-[850px]:hidden">
+    <div class="text-center mt-6 text-3xl font-bold  max-[850px]:w-full text-fBlack max-[450px]:hidden max-[750px]:justify-center max-[750px]:ml-36  ">
         Salma Anika
       </div>
 <Post/>
